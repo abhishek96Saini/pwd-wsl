3a4d02d930f13c118f68517fa5963718844b997a
abhishek96Saini <abhi3112saini@gmail.com>
2025-12-30 15:15:40 +0530

    🐧🪟  add ability to use `pwd-wsl` without pipe in current directory
    The binary size is close to 154 MB after doing a release build. Due to `Foundation` import.
    User must be informed to strip the binary to remove debug metadata.
    
    Not sure if anything more to do in this program.
    	modified:   README.md
    	modified:   Sources/pwd-wsl/pwd_wsl.swift

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-## Status: WIP 🏗️🚧
-- Draft ready. 
-- Need to build as static binary for interoperatability.
+## Status: Almost done 🤓👷🏼👷🏼‍♂️👷🏼‍♀️
+- [x] ~~Draft ready: Currently works via pipe `|`. e.g.: `$> pwd | ~/Downloads/pwd-wsl`~~
+- [x] Need to build as static binary for interoperatability: Built using [Swift static linking SDK](https://www.swift.org/documentation/articles/static-linux-getting-started.html).
+- [x] Allow ability to use the binary via `$PATH` to invoke command on current directory (just like `pwd`): e.g. `~/.local/bin`
+- [ ] Update readme for installation, usage instructions. Also add `strip .build/release/pwd-wsl` as release binary size is close to 150MB 😭.
+- [ ] Publish binary to Git hosting for direct download.
 
-There is another POC implementation in `Python3` in git branch `python-release`
+
+`NOTE:` There is another POC implementation in `Python3` in git branch `python-release`
 
 ## Reference
 https://www.swifttoolkit.dev/posts/pipe
 
-### For `@preconcurrency import Glibc` workaround for `isatty()`
+#### For `@preconcurrency import Glibc` workaround for `isatty()`
 https://github.com/swiftlang/swift/issues/77866
 
-
 https://www.hackingwithswift.com/example-code/strings/replacing-text-in-a-string-using-replacingoccurrencesof
+
+
+#### Get current directory path in CLI
+https://stackoverflow.com/questions/44193114/swift-3-read-terminal-current-working-directory
+
+https://developer.apple.com/documentation/foundation/filemanager/currentdirectorypath
--- Sources/pwd-wsl/pwd_wsl.swift
+++ Sources/pwd-wsl/pwd_wsl.swift
@@ -4,7 +4,7 @@
 
  What the hell? 😭😭
  */
-@preconcurrency import Glibc
+// @preconcurrency import Glibc
 import Foundation
 
 @main
@@ -12,40 +12,22 @@ struct pwd_wsl {
     static func main() throws {
 
         /* Check if program is running inside terminal shell */
-        guard isatty(fileno(stdin)) == 0 else {
-            exit(1)
-        }
+        // guard isatty(fileno(stdin)) == 0 else {
+        //     exit(1)
+        // }
 
         let processInfo: ProcessInfo = ProcessInfo.processInfo
 
         // This works for me in Ubuntu-24.04 WSL.
         let distroName: String? = processInfo.environment["WSL_DISTRO_NAME"]
 
-        /*
-        Use '|' (pipe operator) input to parse and return WSL style path.
-        The intent is to allow POSIX user to pipe `pwd` output and format via
-        this utility in Windows UNC style.
-        */
-        let standardInput: FileHandle = FileHandle.standardInput
-
-        let inputData: Data?
-        do { inputData = try standardInput.readToEnd() } catch {
-            print("ERROR! Cannot read standard input.")
-            exit(1)
-        }
-
-        // output: home/abhi/Developer/my-app
-        var output: String = String(data: inputData ?? Data(), encoding: .utf8) ?? "nil"
+        var output: String = FileManager.default.currentDirectoryPath
 
         output = output.replacingOccurrences(of: "/", with: "\\")
         output =
             if let distroName {
                 "\\\\wsl.localhost\\" + distroName.appending(output)
             } else { output }
-
-        /* output: home/abhi/Developer/my-app
-         What we want: \\\\wsl.localhost\\\(distroName)\\home\\abhi\\Developer\\my-app
-         */
 
         print("\(output)")
 
